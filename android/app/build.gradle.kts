4de0c4969811e5f6192fefb972fcbd68f0e1ff0d
WMs784 <wms784.app@gmail.com>
2025-07-24 03:42:54 +0900

    Upgrade gradle and lints

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.wms784.composite_hunter"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
